7053f30d467b9271cfa134b9f3cd088556d4387f
daseulsong <vates0328@gmail.com>
2020-12-19 00:20:48 +0900

    Add useContext & useEffect Hooks for fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,24 @@ import React from "react";
 import { Store } from "./Store";
 
 export default function App(): JSX.Element {
-  const store = React.useContext(Store);
+  const { state, dispatch } = React.useContext(Store);
+
+  React.useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction();
+  });
+  const fetchDataAction = async () => {
+    const URL =
+      "http://api.tvmaze.com/singlesearch/shows?q=Simpsons&embed=episodes";
+    const data = await fetch(URL);
+    const dataJSON = await data.json();
+    return dispatch({
+      type: "FETCH_DATA",
+      payload: dataJSON._embedded.episodes,
+    });
+  };
+  console.log(state);
   return (
     <>
-      {console.log(store)}
       <h1>The Simpsons</h1>
       <p>Pick your favourite episode!</p>
     </>
--- src/Store.tsx
+++ src/Store.tsx
@@ -14,7 +14,7 @@ const initialState: IState = {
   episodes: [],
   favourites: [],
 };
-export const Store = React.createContext<IState>(initialState);
+export const Store = React.createContext<IState | any>(initialState);
 
 export const reducer = (state: IState, action: IAction): IState => {
   switch (action.type) {
@@ -26,5 +26,10 @@ export const reducer = (state: IState, action: IAction): IState => {
 };
 
 export const StoreProvider = (props: any): JSX.Element => {
-  return <Store.Provider value={initialState}>{props.children}</Store.Provider>;
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  return (
+    <Store.Provider value={{ state, dispatch }}>
+      {props.children}
+    </Store.Provider>
+  );
 };
