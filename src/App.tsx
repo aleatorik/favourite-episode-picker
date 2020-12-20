f0022d1e34b874f30eb3d3f8bd9b99904fd68773
daseulsong <vates0328@gmail.com>
2020-12-20 16:39:31 +0900

    Add an Interface to the element of map function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import "./index.css";
 import { Store } from "./Store";
+
+interface IEpisode {
+  airdate: string;
+  airstamp: string;
+  airtime: string;
+  id: number;
+  image: { medium: string; original: string };
+  name: string;
+  number: number;
+  runtime: number;
+  season: number;
+  summary: string;
+  url: string;
+}
 
 export default function App(): JSX.Element {
   const { state, dispatch } = React.useContext(Store);
@@ -17,14 +32,19 @@ export default function App(): JSX.Element {
       payload: dataJSON._embedded.episodes,
     });
   };
+
+  console.log(state);
+
   return (
     <>
-      <h1>The Simpsons</h1>
-      <p>Pick your favourite episode!</p>
-      <section>
-        {state.episodes.map((episode: any) => {
+      <header className="header">
+        <h1>The Simpsons</h1>
+        <p>Pick your favourite episode!</p>
+      </header>
+      <section className="episode-layout">
+        {state.episodes.map((episode: IEpisode) => {
           return (
-            <section key={episode.id}>
+            <section key={episode.id} className="episode-box">
               <img
                 src={episode.image.medium}
                 alt={`The Simpsons ${episode.name}`}
