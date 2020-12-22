8be69bd2c7d65f277dbbca22f9737a32323429d7
daseulsong <vates0328@gmail.com>
2020-12-22 22:48:21 +0900

    Add two pages as splitting codes from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,9 @@
 import React from "react";
-import "./index.css";
 import { Store } from "./Store";
-import { IAction, IEpisode, IEpisodeProps } from "./interfaces";
 import { Link } from "@reach/router";
 
-const EpisodeList = React.lazy<any>(() => import("./EpisodesList"));
-
-export default function App(): JSX.Element {
-  const { state, dispatch } = React.useContext(Store);
-
-  React.useEffect(() => {
-    state.episodes.length === 0 && fetchDataAction();
-  });
-  const fetchDataAction = async () => {
-    const URL =
-      "https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes";
-    const data = await fetch(URL);
-    const dataJSON = await data.json();
-    return dispatch({
-      type: "FETCH_DATA",
-      payload: dataJSON._embedded.episodes,
-    });
-  };
-
-  const toggleFavAction = (episode: IEpisode): IAction => {
-    const episodeInFav = state.favourites.includes(episode);
-    let dispatchObj = {
-      type: "ADD_FAV",
-      payload: episode,
-    };
-    if (episodeInFav) {
-      const favWithoutEpisode = state.favourites.filter(
-        (fav: IEpisode) => fav.id !== episode.id
-      );
-      dispatchObj = {
-        type: "REMOVE_FAV",
-        payload: favWithoutEpisode,
-      };
-    }
-    return dispatch(dispatchObj);
-  };
-
-  const props: IEpisodeProps = {
-    episodes: state.episodes,
-    toggleFavAction,
-    favourites: state.favourites,
-  };
-
-  console.log(state);
+export default function App(props: any): JSX.Element {
+  const { state } = React.useContext(Store);
 
   return (
     <>
@@ -61,11 +17,7 @@ export default function App(): JSX.Element {
           <Link to="/faves">Favourite(s): {state.favourites.length}</Link>
         </div>
       </header>
-      <React.Suspense fallback={<div>loading ...</div>}>
-        <section className="episode-layout">
-          <EpisodeList {...props} />
-        </section>
-      </React.Suspense>
+      {props.children}
     </>
   );
 }
