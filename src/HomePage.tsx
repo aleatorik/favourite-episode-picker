c3078457c50403cec2cbce0578a43bfbcf0ec5b1
daseulsong <vates0328@gmail.com>
2020-12-24 13:11:58 +0900

    Add a file for all actions

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Store } from "./Store";
-import { IAction, IEpisode, IEpisodeProps } from "./interfaces";
+import { IEpisodeProps } from "./interfaces";
 import "./index.css";
 
 const EpisodeList = React.lazy<any>(() => import("./EpisodesList"));
@@ -11,37 +11,6 @@ export default function HomePage() {
   React.useEffect(() => {
     state.episodes.length === 0 && fetchDataAction();
   });
-
-  const fetchDataAction = async () => {
-    const URL =
-      "https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes";
-    const data = await fetch(URL);
-    const dataJSON = await data.json();
-
-    return dispatch({
-      type: "FETCH_DATA",
-      payload: dataJSON._embedded.episodes,
-    });
-  };
-
-  const toggleFavAction = (episode: IEpisode): IAction => {
-    const episodeInFav = state.favourites.includes(episode);
-    let dispatchObj = {
-      type: "ADD_FAV",
-      payload: episode,
-    };
-    if (episodeInFav) {
-      const favWithoutEpisode = state.favourites.filter(
-        (fav: IEpisode) => fav.id !== episode.id
-      );
-      dispatchObj = {
-        type: "REMOVE_FAV",
-        payload: favWithoutEpisode,
-      };
-    }
-
-    return dispatch(dispatchObj);
-  };
 
   const props: IEpisodeProps = {
     episodes: state.episodes,
